3bd964a567df9f0b789162bcd76697c0a2420ed0
oat431 <oat431@gmail.com>
2023-07-17 23:49:38 +0700

    🛠 setup: spb3 kafka proj

---
+++ src/main/java/panomete/spb3kafka/Spb3kafkaApplication.java
@@ -0,0 +1,13 @@
+package panomete.spb3kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb3kafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spb3kafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/panomete/spb3kafka/Spb3kafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.spb3kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spb3kafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
